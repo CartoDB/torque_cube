67cac6ad0981c1fd926cad91606386de489694c2
Sandro Santilli <strk@keybit.net>
2013-05-02 15:29:35 +0200

    Add time slot component into the pyramid

--- pyramid.sql
+++ pyramid.sql
@@ -1,38 +1,50 @@
 -- {
-CREATE OR REPLACE FUNCTION _CDB_BuildPyramid_Tile(tbl regclass, col text, tile_ext geometry, tile_res float8)
-RETURNS TABLE(ext geometry, v int)
+CREATE OR REPLACE FUNCTION _CDB_BuildPyramid_Tile(tbl regclass, col text, res float8, originX float8, originY float8, tcol text, tres integer, torigin integer)
+RETURNS TABLE(ext geometry, t timestamp, c int)
 AS $$
 DECLARE
   rec RECORD;
   sql text;
   cell text;
-  originX float;
-  originY float;
+  tcell text;
+  tslot text;
   tile_totcount integer;
 BEGIN
 
-  originX := st_xmin(tile_ext) - tile_res/2.0;
-  originy := st_ymin(tile_ext) - tile_res/2.0;
-
   cell := 'ST_SnapToGrid(' || quote_ident(col) || ', '
       || originX || ',' || originY || ','
-      || tile_res || ',' || tile_res || ')'
+      || res || ',' || res || ')'
       ;
 
+  tcell := '''epoch''::timestamp + ( '
+      || torigin
+      || ' + round( ( extract(epoch from ' || quote_ident(tcol) || ')-'
+      || torigin || ') / ' || tres || ') * ' || tres
+      || ') * ''1s''::interval';
+
+  -- TODO: use a diagonal line for the cell extent ?
   sql := 'SELECT ST_Envelope(ST_Buffer('
-      || cell || ',' || (tile_res/2.0) || ', 1)) as ext, count('
+      || cell || ',' || (res/2.0) || ', 1)) as ext, ';
+  IF tcol IS NOT NULL THEN
+    sql := sql || tcell || ' as t, ';
+  END IF;
+  sql := sql
+      || 'count('
       || quote_ident(col) || ') FROM ' || tbl::text
-      || ' i WHERE i.' || quote_ident(col)
-      || ' && ' || quote_literal(tile_ext::text)
-      || '::geometry GROUP BY ' || cell
-      ;
+      || ' GROUP BY ' || cell;
+  IF tcol IS NOT NULL THEN
+    sql := sql || ',  t';
+  END IF;
 
-  --RAISE DEBUG 'Query: %', sql;
+  RAISE DEBUG 'Query: %', sql;
 
   FOR rec IN  EXECUTE sql LOOP
     --RAISE DEBUG 'Count in macropixel %,%:%', rec.x, rec.y, rec.count;
     ext := rec.ext;
-    v := rec.count;
+    IF tcol IS NOT NULL THEN
+      t := rec.t;
+    END IF;
+    c := rec.count;
     RETURN NEXT;
   END LOOP;
 
@@ -41,7 +53,7 @@ $$
 LANGUAGE 'plpgsql'; -- }
 
 -- {
-CREATE OR REPLACE FUNCTION CDB_BuildPyramid(tbl regclass, col text)
+CREATE OR REPLACE FUNCTION CDB_BuildPyramid(tbl regclass, col text, tcol text DEFAULT NULL)
 RETURNS void AS
 $$
 DECLARE
@@ -49,15 +61,17 @@ DECLARE
   ptab text; -- pyramids table
   maxpix integer; -- max pixels 
   tblinfo RECORD;
+  tbltinfo RECORD; -- table time info
   tile_ext geometry;
   tile_res float8;
   prev_tile_res float8;
   rec RECORD;
   pixel_vals integer;
+  ntslots integer;
 BEGIN
 
   -- Setup parameters 
-  maxpix := 16383; -- 32767; -- 65535; 
+  maxpix := 16383; 
 
   -- Extract table info
   WITH info AS (
@@ -73,9 +87,25 @@ BEGIN
 
   RAISE DEBUG '"%"."%" ext is %', tblinfo.nsp, tblinfo.tab, tblinfo.ext;
 
+  ntslots := 16;
+  IF tcol IS NOT NULL THEN
+    sql := 'SELECT min(' || quote_ident(tcol) || '), max('
+      || quote_ident(tcol) || '), NULL::integer as res FROM ' || tbl;
+    RAISE DEBUG '%', sql;
+    EXECUTE sql INTO tbltinfo;
+    tbltinfo.res := ceil(extract(epoch from tbltinfo.max - tbltinfo.min) / ntslots);
+
+    RAISE DEBUG 'Time resolution: % seconds', tbltinfo.res;
+
+  END IF;
+
   -- 1. Create the pyramid table 
   ptab := quote_ident(tblinfo.nsp) || '."' || tblinfo.tab || '_pyramid' || '"';
-  sql := 'CREATE TABLE ' || ptab || '(res float8, ext geometry, c int)';
+  sql := 'CREATE TABLE ' || ptab || '(res float8, ext geometry, tres integer, ';
+  IF tcol IS NOT NULL THEN
+      sql := sql || ' t timestamp,';
+  END IF;
+  sql := sql || 'c int)';
   BEGIN
     EXECUTE sql;
   EXCEPTION 
@@ -90,20 +120,32 @@ BEGIN
   tile_ext := ST_SetSRID(tblinfo.ext::geometry, tblinfo.srid);
   tile_res := least(st_xmax(tile_ext)-st_xmin(tile_ext), st_ymax(tile_ext)-st_ymin(tile_ext)) / 2048; 
 
+  -- TODO: re-compute tile_ext to always be the full webmercator extent
+  --       or better yet take it as a parameter
+
+  -- TODO: compute upper levels from lower ones, using ST_Covers
+
   LOOP
 
     sql := 'INSERT INTO ' || ptab
-        || '(res, ext, c) SELECT ' 
-        || tile_res || ', ext, v FROM _CDB_BuildPyramid_Tile('
+        || '(res, ext, t, c) SELECT '
+        || tile_res || ', ext, t, c FROM _CDB_BuildPyramid_Tile('
         || quote_literal(tbl) || ',' || quote_literal(col::text)
-        || ',' || quote_literal(tile_ext::text)
-        || ', ' || tile_res || ')';
+        || ',' || tile_res || ', ' || st_xmin(tile_ext) - tile_res/2.0 || ', '
+        || st_ymin(tile_ext) - tile_res/2.0;
+    IF tcol IS NOT NULL THEN
+        sql := sql || ', ' || quote_literal(tcol) || ','
+          || tbltinfo.res || ',' || floor(extract(epoch from tbltinfo.min));
+    END IF;
+    sql := sql || ')';
 
-    -- RAISE DEBUG '%', sql;
+    RAISE DEBUG '%', sql;
 
     EXECUTE sql;
 
-    GET DIAGNOSTICS pixel_vals := ROW_COUNT;
+    sql := 'SELECT count(distinct ext) FROM ' || ptab || ' WHERE res = ' || tile_res;
+    EXECUTE sql INTO pixel_vals;
+    --GET DIAGNOSTICS pixel_vals := ROW_COUNT;
 
     RAISE DEBUG '% pixels with resolution %', pixel_vals, tile_res;
 
--- pyramid_test.sql
+++ pyramid_test.sql
@@ -1,27 +1,27 @@
 \i pyramid.sql
 DROP TABLE IF EXISTS istituti_pyramid CASCADE;
-SELECT CDB_BuildPyramid('istituti', 'the_geom_webmercator');
+SELECT CDB_BuildPyramid('istituti', 'the_geom_webmercator', 'created_at');
 
--- Print summary of what's been constructed
-select round(ST_xmin(st_extent(ext))::numeric, 0) as x0, round(st_xmax(st_extent(ext))::numeric, 0) as x1, round(st_ymin(st_extent(ext))::numeric, 0) as y0, round(st_ymax(st_extent(ext))::numeric, 0) as y1, res, sum(c), count(*)
-from istituti_pyramid
-group by res
-order by res desc;
+-- Print summary of pixels
+select res, sum(c), count(*) from istituti_pyramid
+group by res order by res desc;
 
--- Fetch tiles for tile 1,0,1
-SELECT count(*) FROM istituti_pyramid p
- WHERE p.res = ( select max(res) from istituti_pyramid
-                 where res < CDB_XYZ_Resolution(1) )
-   AND p.ext && CDB_XYZ_Extent(1, 0, 1);
+-- Print summary of time slots per resolution
+select res, count(distinct t) from istituti_pyramid
+group by res order by res;
 
--- Fetch tiles for tile 2,1,2
-SELECT count(*) FROM istituti_pyramid p
- WHERE p.res = ( select max(res) from istituti_pyramid
-                 where res < CDB_XYZ_Resolution(2) )
-   AND p.ext && CDB_XYZ_Extent(2, 1, 2);
-
--- Fetch tiles for tile 5,4,3
-SELECT count(*) FROM istituti_pyramid p
+-- Count pixels for tile 5,4,3
+SELECT count(distinct ext) FROM istituti_pyramid p
  WHERE p.res = ( select max(res) from istituti_pyramid
                  where res < CDB_XYZ_Resolution(3) )
    AND p.ext && CDB_XYZ_Extent(5, 4, 3);
+
+-- Fetch pixels for tile 5,4,3
+SELECT
+  st_xmin(ext) as x, st_ymin(ext) as y,
+  array_agg(extract(epoch from t)::text || ':' || c::text) as v
+  FROM istituti_pyramid p
+  WHERE p.res = ( select max(res) from istituti_pyramid
+                 where res < CDB_XYZ_Resolution(3) )
+   AND p.ext && CDB_XYZ_Extent(5, 4, 3)
+  GROUP BY ext;
